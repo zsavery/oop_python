c349b55d0eee116c03e8ad42a032863b736bf6c4
zsavery <zsavery998@gmail.com>
2025-07-26 16:02:13 -0500

    lesson 1

--- VideoGameCharacter.py
+++ VideoGameCharacter.py
@@ -1,11 +1,16 @@
 class VideoGameCharacter:
     def __init__(self, name="Player", level=0, health=100, skills=[], 
                  item_capacity=1, position= (0,0,0)):
-            self.name = name
-            self.level = level
-            self.health = health
-            self.skills = skills
-            self.item_capacity = item_capacity
-            self.position = position
+            self._name = name
+            self._level = level
+            self._health = health
+            self._skills = skills
+            self._item_capacity = item_capacity
+            self._position = position
     
-    
+    def set_name(self, n):
+          self._name = n
+
+    @property
+    def name(self):
+          return self._name
